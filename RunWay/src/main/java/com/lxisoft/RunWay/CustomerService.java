71677b82b7eb77151d1e71e79baeb023210e4743
Pavana <pavana.n.p@lxisoft.com>
2019-12-05 16:46:48 +0530

    IN-3070

--- RunWay/src/main/java/com/lxisoft/RunWay/CustomerService.java
+++ RunWay/src/main/java/com/lxisoft/RunWay/CustomerService.java
@@ -3,11 +3,17 @@ package com.lxisoft.RunWay;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class CustomerService {
 @Autowired
 CustomerRepository repo;
 public Customer saveCustomer(Customer customer) {
 	return repo.save(customer);
+}
+
+public Customer getCustomerById(Long id) {
+    java.util.Optional<Customer> optionalcustomer = repo.findById(id);
+    return optionalcustomer.get();
 }
 }
