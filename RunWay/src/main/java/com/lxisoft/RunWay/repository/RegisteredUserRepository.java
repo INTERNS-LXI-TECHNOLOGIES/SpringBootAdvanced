7928f01759648436aa2fbd2c48f6cb86ee59c4a0
Pavana <pavana.n.p@lxisoft.com>
2019-12-09 13:44:38 +0530

    IN-3235

--- RunWay/src/main/java/com/lxisoft/RunWay/repository/RegisteredUserRepository.java
+++ RunWay/src/main/java/com/lxisoft/RunWay/repository/RegisteredUserRepository.java
@@ -3,9 +3,9 @@ package com.lxisoft.RunWay.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.lxisoft.RunWay.model.LoginUser;
+import com.lxisoft.RunWay.model.RegisteredUser;
 
 @Repository
-public interface LoginUserRepository extends JpaRepository<LoginUser,Long>{
+public interface RegisteredUserRepository extends JpaRepository<RegisteredUser,Long>{
 
 }
