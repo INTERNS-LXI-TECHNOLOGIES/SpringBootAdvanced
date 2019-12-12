496feee2877a5027e38c4efb6d86295d7ffa1e46
Ajay1729 <ajay.e.s@lxisoft.com>
2019-12-12 16:43:49 +0530

    IN-3486

--- RunWay/src/main/java/com/lxisoft/RunWay/controller/WebController.java
+++ RunWay/src/main/java/com/lxisoft/RunWay/controller/WebController.java
@@ -1,16 +1,22 @@
 package com.lxisoft.RunWay.controller;
 
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.lxisoft.RunWay.model.Customer;
 import com.lxisoft.RunWay.model.Owner;
@@ -61,13 +67,28 @@ public String showOwnerForm(Owner owner) {
 	        return "redirect:/ownerregister?success";
 	}
 @RequestMapping("/CustomerHome")
-public String getVehicle(Model model ) {
-		  
-    
-    List<Vehicle> vehicleList = vehControl.readVehicleDetails();
-     
- model.addAttribute("vehicleList", vehicleList);
-  
+public String getVehicle(Model model,@ModelAttribute String type ) {
+	 String user="ajay";
+	 String pass="ajay";
+	 model.addAttribute("user", user);
+	 model.addAttribute("pass", pass);
  return "CustomerHome" ;
 }
+ @GetMapping("/CustomerHome/{type}/{date}")
+ public String get(Model model,@PathVariable String type ,@PathVariable String date) {
+	 Date date1=new Date();  
+	 try {
+		  date1=new SimpleDateFormat("yyyy/MM/dd").parse(date);
+		   
+	} catch (ParseException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}  
+	 List<Vehicle> vehicleList = vehControl.readVehicleDetails(type,date1 );
+
+	 model.addAttribute("vehicleList", vehicleList);
+	 
+	 return "fragments/myFragment.html::myFragment";
+	 
+ }
 }
--- RunWay/src/main/java/com/lxisoft/RunWay/repository/VehicleRepository.java
+++ RunWay/src/main/java/com/lxisoft/RunWay/repository/VehicleRepository.java
@@ -1,11 +1,17 @@
 package com.lxisoft.RunWay.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.lxisoft.RunWay.model.Vehicle;
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
-	
-
+	 
+	@Query (value="select vehicle.id,vehicle.mileage,vehicle.model,vehicle.owner_id,vehicle.price,vehicle.seats,vehicle.type from vehicle,booked_detail where vehicle.type='CAR' and booked_detail.vehicle_id!=vehicle.id ");
+	public List<Vehicle> getVehicle(@Param("type")String type,@Param("date") Date date);
 }
