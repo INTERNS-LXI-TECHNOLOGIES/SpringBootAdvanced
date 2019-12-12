62ff280d4737013a752e6d3440e66286b3094d66
Pavana <pavana.n.p@lxisoft.com>
2019-12-12 14:47:56 +0530

    IN-3430

--- RunWay/src/main/java/com/lxisoft/RunWay/controller/RegisteredUserController.java
+++ RunWay/src/main/java/com/lxisoft/RunWay/controller/RegisteredUserController.java
@@ -1,16 +1,19 @@
 package com.lxisoft.RunWay.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import com.lxisoft.RunWay.model.RegisteredUser;
 import com.lxisoft.RunWay.service.RegisteredUserService;
 
 @RestController
 public class RegisteredUserController {
 @Autowired
 RegisteredUserService service;
-
+@PostMapping("/customerregister")
+public RegisteredUser saveOwner(@RequestBody RegisteredUser user) {
+	return service.saveRegisteredUser(user);
+}
 }
