c17494fbf8d8d0143f94d65ff909600984ff2860
ashiq k p <muhammed.ashiq.k.p@lxisoft.com>
2020-02-01 15:46:09 +0530

    http://jira.bytatech.com:8080/browse/IN-5809

--- ContactApp/src/main/java/com/lxisoft/controller/ContactController.java
+++ ContactApp/src/main/java/com/lxisoft/controller/ContactController.java
@@ -1,15 +1,28 @@
 package com.lxisoft.controller;
+import com.lxisoft.service.*;
+import com.lxisoft.domain.*;
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ContactController 
 {	
-	@RequestMapping(value="/add")
+	{
+		System.out.println("controller");
+	}
+	
+//	@Autowired
+//	Service service;
+	@RequestMapping(value="/showAll")
 	public String findAll()
 	{
+//		ArrayList<Contact> contactList=((Object) service).findAllService();
 		System.out.println("dddddddddd");
-	      return "Fail";
+	      return "ViewAll";
+	      
 	}
 //	@RequestMapping(value="/", method = RequestMethod.GET)
 //	public String findAll()
