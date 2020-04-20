4c5aef2001264a12088333a3548c50f0c33e18e3
amritha <amritha.p.m@lxisoft.com>
2020-04-20 12:49:57 +0530

    http://jira.bytatech.com:8080/browse/IN-9234

---
+++ MockExam/src/main/java/com/lxisoft/config/core/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.lxisoft.config.core;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
---
+++ MockExam/src/main/java/com/lxisoft/web/controller/MockController.java
@@ -0,0 +1,60 @@
+package com.lxisoft.web.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MockController {
+
+	@RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
+	public ModelAndView welcomePage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Welcome to Mockexam");
+		model.addObject("message", "This is welcome page!");
+		model.setViewName("hello");
+		return model;
+
+	}
+
+	
+	@RequestMapping(value = "/main",method = RequestMethod.GET)
+	public String getUser()
+	{
+		/*
+		 * Authentication authentication =
+		 * SecurityContextHolder.getContext().getAuthentication(); boolean hasUserRole =
+		 * authentication.getAuthorities().stream() .anyMatch(r ->
+		 * r.getAuthority().equals("ROLE_ADMIN"));
+		 * 
+		 * if(hasUserRole) { return "home"; } else
+		 */
+		
+			return "Instruction";
+		
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login(@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout) {
+
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject("error", "Invalid username and password!");
+		}
+
+		if (logout != null) {
+			model.addObject("msg", "You've been logged out successfully.");
+		}
+		model.setViewName("login");
+
+		return model;
+
+	}
+
+}
