16b0f4e3ed0acb887693b0b3fc521fa4ec2fd7e3
megha-vt <megha.v.t@lxisoft.com>
2020-04-21 19:23:11 +0530

    http://jira.bytatech.com:8080/browse/IN-9363

--- MockExam/src/main/java/com/lxisoft/config/SecurityConfig.java
+++ MockExam/src/main/java/com/lxisoft/config/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("mkyong").password("123456").roles("USER");
-		auth.inMemoryAuthentication().withUser("admin").password("123456").roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("index").password("123456").roles("ADMIN");
 		auth.inMemoryAuthentication().withUser("dba").password("123456").roles("DBA");
 	}
 
@@ -22,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http.authorizeRequests()
-			.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+			.antMatchers("/index/**").access("hasRole('ROLE_ADMIN')")
 			.antMatchers("/dba/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_DBA')")
 			.and().formLogin();
 		
