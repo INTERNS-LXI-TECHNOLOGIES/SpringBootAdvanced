91988f777b40e6531704e398f4a6a4c6cbbfd110
gokullxi <gokul.t.s@lxisoft.com>
2020-04-24 20:00:02 +0530

    http://jira.bytatech.com:8080/browse/IN-9510

--- Hackathon/Covid19AffidavitRequester/affidavitRequester/src/main/java/com/gokul/config/SpringSecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.gokul.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
--- Hackathon/Covid19AffidavitRequester/affidavitRequester/src/main/java/com/gokul/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.gokul.exception;
-
-public class ResourceNotFoundException extends Exception {
-    private static final long serialVersionUID = 1 ;
-
-    public ResourceNotFoundException(Object resourId) {
-        super(resourId != null ? resourId.toString() : null);
-    }
-}
