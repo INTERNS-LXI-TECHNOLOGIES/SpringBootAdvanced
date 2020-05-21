1387d0eed2933377f29eb493b04850e9d8fe7811
gokullxi <gokul.t.s@lxisoft.com>
2020-05-21 18:25:23 +0530

    r and d on spring boot

---
+++ TestProject/helloworld/src/main/java/com/lxisoft/sample/services/HelloWorldServices.java
@@ -0,0 +1,17 @@
+package com.lxisoft.sample.services;
+
+import org.springframework.stereotype.Service;
+
+@Service("helloWorldService")
+public class HelloWorldServices {
+ 
+	private String name;
+ 
+	public void setName(String name) {
+		this.name = name;
+	}
+ 
+	public String sayHello() {
+		return "Hello! " + name;
+	}
+}
